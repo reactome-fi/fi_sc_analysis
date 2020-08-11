447b841b231c6e57c293bac5726bda26c37d8d36
teslajoy <sanati@ohsu.edu>
2020-08-11 14:24:35 -0700

    initial skeleton of setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+__version__ = '0.0.1'
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='name of package',
+    version=__version__,
+    description='python service for single cell analysis in reactome',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/reactome-fi/fi_sc_analysis',
+    author='Guanming Wu',
+    author_email='wu@ohsu.edu',
+    # which license ?
+    license='Apache',
+    packages=find_packages(),
+    install_requires=[
+        '',
+    ],
+    extras_require={
+        # fill in versions
+        'pandas': ['pandas=='],
+        'numpy': ['numpy=='],
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'
+    ],
+    platforms=['any'],
+    # which python version
+    python_requires='>=3.8',
+)
