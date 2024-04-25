7c98a83fd35335b372c73112d379c3358a006826
guanmingwu <guanmingwu@gmail.com>
2024-04-25 16:20:46 -0700

    added functions to summarize networks via llm

--- python/scpy4reactome/regulatory_network_constructor.py
+++ python/scpy4reactome/regulatory_network_constructor.py
@@ -21,6 +21,8 @@ import py4cytoscape as p4c
 import py4cytoscape.networks as pcn
 import py4cytoscape.styles as pcs
 from pathlib import Path
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 
 
 def analyze_tf_pathway_correlations_via_spearman(adata: sc.AnnData,
@@ -842,7 +844,6 @@ def write_network_to_graphml(network: nx.DiGraph,
     nx.write_graphml(network, filename)
 
 
-
 def open_graphml_in_cytoscape(graphml_file: str,
                               layout: str = None):
     """Open a graphml network in Cytoscape desktop.
@@ -870,3 +871,101 @@ def open_graphml_in_cytoscape(graphml_file: str,
     p4c.set_visual_style(style_name)
     if layout is not None:
         p4c.layout_network(layout)
+
+
+def extract_information(network: nx.DiGraph | str) -> tuple[list, list, list, list]:
+    """Extract transcription factors, pathways, ligands, and their relationships from the network.
+
+    Args:
+        network (nx.DiGraph): _description_
+
+    Returns:
+        tuple[list, list, list, list]: four lists in the order of tfs, pathways, ligands and relationships.
+    """
+    # Need to load the network first if it is a file
+    if isinstance(network, str):
+        path = Path(network)
+        if not path.exists():
+            raise ValueError('No graphml file exists at: {}'.format(network))
+        network = nx.read_graphml(network)
+    tfs = []
+    pathways = []
+    ligands = []
+    fis = [] # functional relationships among nodes
+    for node, data in network.nodes(data=True):
+        type = data['type']
+        if type == 'TF':
+            tfs.append(node)
+        elif type == 'Pathway':
+            pathways.append(node)
+        elif type == 'Ligand':
+            ligands.append(node)
+        else:
+            raise ValueError('Unknown type: {}'.format(type))
+    
+    # Extract edge information
+    # Mapping action for different edge types
+    edge_annotation_actions = {
+        'ligand_pathway_activation': lambda _ : "activates",
+        'tf_pathway_inhibition': lambda _ : 'inhibits',
+        'tf_tf_activation': lambda _ : 'activates',
+        'tf_pathway_activation': lambda _ : 'activates',
+        'pathway_tf_annotation': lambda _ : 'regulates',
+        'pathway_pathway_hierarchy': lambda _ : 'is contained by',
+    }
+    for src, target, data in network.edges(data=True):
+        annotation = data['annotation']
+        edge_meaning = edge_annotation_actions[annotation]
+        if edge_meaning is None:
+            raise ValueError('No meaning defined for edge: {}'.format(annotation))
+        fi = '"{}" {} "{}"'.format(src, edge_meaning(annotation), target) # The second element calls the function
+        fis.append(fi)
+
+    return tfs, pathways, ligands, fis
+
+
+def get_llm_summary_prompt_template():
+    network_summary_prompt_template = """
+You are an expert in the field of {study_context}. Relying on the provided lists for reference, use your domain knowledge and write 
+some paragraphs in a scholar way to highlight the most important pathways and their functional relationships with transcription 
+factors and ligands with no more than {total_words} words. Be specific and don't speculate!
+
+transcription factors: {transcriptaion_factors}
+
+pathways: {pathways}
+
+ligands: {ligands}
+
+functional relationships: {functional_relationships}
+
+"""
+    network_summary_prompt = ChatPromptTemplate.from_template(network_summary_prompt_template)
+    return network_summary_prompt
+
+
+def summarize_network_via_llm(network: nx.DiGraph,
+                              study_context: str,
+                              model: any,
+                              total_words: int = 500) -> any:
+    tfs, pathways, ligands, fis = extract_information(network)
+    # print('TFs: {}\nPathways: {}\nLigands: {}\nFIs: {}'.format(tfs, pathways, ligands, fis))
+    llm_parameters = {
+        'transcriptaion_factors': ';'.join(tfs),
+        'pathways': ';'.join(pathways),
+        'ligands': ';'.join(ligands),
+        'functional_relationships': ';'.join(fis),
+        'total_words': total_words
+    }
+
+    # Add a context about that this study is for
+    llm_parameters['study_context'] = study_context
+    network_summary_prompt = get_llm_summary_prompt_template()
+    answer = {
+        'answer': network_summary_prompt | model 
+    }
+
+    dummy = RunnablePassthrough()
+    llm_chain = dummy | answer
+    result = llm_chain.invoke(llm_parameters)
+    return result
+
